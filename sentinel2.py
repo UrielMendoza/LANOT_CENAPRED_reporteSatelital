688a5c294279c0c5e9ea04e955a7bd800470df45
UrielMendoza <urielmendozacastillo@gmail.com>
2021-09-06 23:55:38 -0500

    Update sentinel2.py

--- sentinel2.py
+++ sentinel2.py
@@ -8,5 +8,5 @@ end_date = datetime.datetime.now()
 region = 'volcanes'
 tiles = base.tiles[region]
 #download_datasets.search_and_download_datasets(tiles, start_date, end_date, pathOutputL2A, unzip=False)
-daysDelta = 1
+daysDelta = 0
 download_datasets.search_and_download_datasets(tiles, start_date - datetime.timedelta(days=daysDelta), end_date - datetime.timedelta(days=daysDelta), pathOutputL2A, unzip=False)
